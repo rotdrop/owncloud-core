5a65f1fe4a450e18e718e364a3e2a65ffa481256
Claus-Justus Heine <himself@claus-justus-heine.de>
2015-07-06 22:37:11 +0200

    Hack around Firefox auto-fill greediness: IT IS JUST NOT A GOOD IDEA TO AUTOFILL PASSWORDS INSIDE THE PASSWORD RESET DIALOG.

--- core/lostpassword/templates/resetpassword.php
+++ core/lostpassword/templates/resetpassword.php
@@ -11,6 +11,7 @@ script('core', [
 <form action="<?php print_unescaped($_['link']) ?>" id="reset-password" method="post" autocomplete="off">
 	<fieldset>
 		<p>
+			<input type="password" style="display:none">
 			<label for="password" class="infield"><?php p($l->t('New password')); ?></label>
 			<input type="password" name="password" id="password" value="" placeholder="<?php p($l->t('New Password')); ?>" required data-typetoggle="#password-show"/>
 			<img class="svg" id="password-icon" src="<?php print_unescaped(image_path('', 'actions/password.svg')); ?>" alt=""/>
